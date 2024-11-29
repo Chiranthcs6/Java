e2fbcd53491133ab14edb115c29c499502d0545c
Chiranthcs5@gmail.com <Chiranthcs5@gmail.com>
2024-11-29 11:43:34 +0530

    multithreading

---
+++ TestmyThread.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread{
+    MyThread(){
+        super("Using Thread class");
+        System.out.println("This is child thread:"+this);
+        start();
+    }
+    public void run(){
+        try{
+            for(int i=5;i>0;i--){
+                System.out.println("Child Thread"+i);
+                Thread.sleep(500);
+            } 
+        }catch(InterruptedException e){}
+        System.out.println("Exiting Child class");
+    }
+}
+class TestmyThread{
+    public static void main(String[] args){
+        new MyThread();
+        try{
+            for(int k=5;k<0;k--){
+                System.out.println("Running main thread"+k);
+                Thread.sleep(1000);
+            }
+        }catch(InterruptedException e){}
+        System.out.println("Exiting main method:");
+    }
+}
