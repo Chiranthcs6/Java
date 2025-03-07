3414cba253c9f373cdd3d46ba25dbddb63d98025
chiranthcs06 <chiranthcs5@gmail.com>
2025-03-07 15:50:30 +0530

    ca

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -1,3 +1,18 @@
+import java.util.HashSet;
 public class FindDuplicate {
-  
+  public static void findduplicate(int[] arr){
+    HashSet<Integer>seen= new HashSet<>();
+    HashSet<Integer> duplicate= new HashSet<>();
+
+    for(int num:arr){
+      if(!seen.add(num)){
+        duplicate.add(num);
+      }
+    }
+    System.out.println("Duplicate elemet:"+duplicate);
+  }
+  public static void main(String[] args) {
+      int[] arr={1,1,2,4,5,6,7,7,8,2,21};
+      findduplicate(arr);
+  }
 }
